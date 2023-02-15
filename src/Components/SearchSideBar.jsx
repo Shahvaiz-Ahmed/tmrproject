9a4f9897f63df4ef8852d16713e2621b3392c9e3
Malik Abdullah Naazar <malikabdullah130037@example.com>
2023-02-15 13:02:58 +0500

    design sidesearh bar with material subtypes types and complainces

--- src/Components/SearchSideBar.jsx
+++ src/Components/SearchSideBar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './Static/css/SearchSideBar.css';
 import product from './Static/images/product.png';
 import Button from '@mui/material/Button';
+import '@djthoms/pretty-checkbox';
 
 
 function SearchSideBar() {
@@ -9,46 +10,103 @@ function SearchSideBar() {
     const [csUpper, setcsUpper] = useState();
     const [idLower, setidLower] = useState();
     const [idUpper, setidUpper] = useState();
-  
+
     const handleSizeValues = () => {
-      setcsLower(document.querySelector('#cs-lower').value);
-      setcsUpper(document.querySelector('#cs-upper').value);
-      setidLower(document.querySelector('#id-lower').value);
-      setidUpper(document.querySelector('#id-upper').value);
+        setcsLower(document.querySelector('#cs-lower').value);
+        setcsUpper(document.querySelector('#cs-upper').value);
+        setidLower(document.querySelector('#id-lower').value);
+        setidUpper(document.querySelector('#id-upper').value);
     };
     useEffect(() => {
         console.log(csLower, csUpper, idLower, idUpper);
-      }, [csLower, csUpper, idLower, idUpper]);
-      
+    }, [csLower, csUpper, idLower, idUpper]);
 
-  return (
-    <div className="sidebar">
-        <img src={product} alt="product" />
-        <Button variant="contained" type="submit" onClick={handleSizeValues}>Contained</Button>
-        <h2><strong>Switch</strong> between mm and in</h2>
-        <div>
+
+    return (
+        <div className="sidebar">
+            <img src={product} alt="product" />
+            <h2><strong>Switch</strong> between mm and in</h2>
             <div>
-                <p></p>
-                <p>Lower</p>
-                <p></p>
-                <p>Upper</p>
+                <div>
+                    <p></p>
+                    <p>Lower</p>
+                    <p></p>
+                    <p>Upper</p>
+                </div>
+                <div>
+                    <p>CS(mm)</p>
+                    <input type="search" id='cs-lower' />
+                    <p>to</p>
+                    <input type="search" id='cs-upper' />
+                </div>
+                <div>
+                    <p>ID(mm)</p>
+                    <input type="search" id='id-lower' />
+                    <p>to</p>
+                    <input type="search" id='id-upper' />
+                </div>
+                <Button className="btn" variant="contained" type="submit" onClick={handleSizeValues}>Contained</Button>
+                <div className="material-type">
+                    Material Type
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
+                <div className="material-type">
+                    Material Sub-type
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
+                <div className="material-type">
+                    Material Compliances
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
+                <div className="mtchecck-box">
+                    <input type="checkbox" name="mmycheck   " id="1" />
+                    <label htmlFor="1">QUALITY RUBBER</label>
+                </div>
             </div>
-            <div>
-                <p>CS(mm)</p>
-                <input type="search" id='cs-lower'/>
-                <p>to</p>
-                <input type="search" id='cs-upper'/>
-            </div>
-            <div>
-                <p>ID(mm)</p>
-                <input type="search" id='id-lower'/>
-                <p>to</p>
-                <input type="search" id='id-upper'/>
-            </div>
+
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default SearchSideBar
