cee3802a7b8d24360fdad40fcfec15bfcd352314
Shahvaiz-Ahmed <91520897+Shahvaiz-Ahmed@users.noreply.github.com>
2023-02-16 10:29:13 +0500

    improved navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import DetailedDescription from './Components/DetailedDescription';
+import Menu from './Components/Menu';
 import Navbar from './Components/Navbar';
 import ProductComponent from './Components/ProductComponent';
 import RecomendedItems from './Components/RecomendedItems';
@@ -8,9 +11,22 @@ import RecomendedItems from './Components/RecomendedItems';
 // import SearchTable from './Components/SearchTable';
 
 function App() {
+  const [sideMenu, setsideMenu] = useState(false);
+
+  useEffect(() => {  
+    return () => {
+      window.addEventListener('scroll', ()=>{ setsideMenu(false) });
+    }
+  }, [])
+  
+
   return (
+    
     <div className="App">
-      <Navbar/>
+      {
+        sideMenu?<Menu sideMenu={sideMenu} setsideMenu={setsideMenu}/>:<></>
+      }
+      <Navbar sideMenu={sideMenu} setsideMenu={setsideMenu}/>
       <ProductComponent/>
       <RecomendedItems/>
       <DetailedDescription/>
