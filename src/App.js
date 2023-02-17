91e34fa0c2fd3e65760b83328a9f340bb0a66f4c
Malik Abdullah <malikabdullah130037@example.com>
2023-02-17 11:03:24 +0500

    sloved home page error

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import './App.css';
 import AllComponents from './Components/Allcomponent';
 import SearchSideBar from './Components/SearchSideBar';
 import SearchTable from './Components/SearchTable';
-
+import Menu from './Components/Menu';
+import Navbar from './Components/Navbar';
+import { useEffect } from 'react';
+import { useState } from 'react';
 function App() {
+  const [sideMenu, setsideMenu] = useState(false);
 
+  useEffect(() => {  
+    return () => {
+      window.addEventListener('scroll', ()=>{ setsideMenu(false) });
+    }
+  }, [])
   return (
     <Routes>
       <Route path="/Product" element={<AllComponents />}/>
-      <Route path="/" element={<><div className='search-body'>
+      <Route path="/" element={<>
+        {sideMenu ? <Menu sideMenu={sideMenu} setsideMenu={setsideMenu} /> : <></>}
+      <Navbar sideMenu={sideMenu} setsideMenu={setsideMenu} />
+      <div className='search-body'>
        <SearchSideBar/>   <SearchTable/>
        </div>
         </>}/>
